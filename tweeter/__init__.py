516f8d0ab55f138672f8ba7f1da3ae3979d0cf26
Ahmed Javed <ahmedjaved701@gmail.com>
2020-08-25 01:26:52 +0500

    Bug fixes

--- tweeter/__init__.py
+++ tweeter/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dcef3fd2f3a917dda52f56ed46ddea37'
 app.config['DATABASE_URL'] = 'postgresql:///tweeter'
@@ -12,6 +14,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+import click
+from flask.cli import with_appcontext
+
+from tweeter.models import User, Tweet
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
 
 
 from tweeter import routes
