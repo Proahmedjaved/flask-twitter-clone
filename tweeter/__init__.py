5f1d10db9b8fa8acdca4693d36e626e790063610
Ahmed Javed <ahmedjaved701@gmail.com>
2020-08-25 01:48:58 +0500

    bug fixes

--- tweeter/__init__.py
+++ tweeter/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,22 +8,13 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dcef3fd2f3a917dda52f56ed46ddea37'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tweeter'
-db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-import click
-from flask.cli import with_appcontext
-
-from tweeter.models import User, Tweet
-
-@click.command('create-tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
 
 
 from tweeter import routes
--- tweeter/commands.py
+++ tweeter/commands.py
@@ -1,11 +1,11 @@
-# import click
-# from flask.cli import with_appcontext
+import click
+from flask.cli import with_appcontext
 
-# from tweeter import db
-# from tweeter.models import User, Tweet
+from tweeter import db
+from tweeter.models import User, Tweet
 
 
-# @click.command(name='create_tables')
-# @with_appcontext
-# def create_tables():
-#     db.create_all()
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
