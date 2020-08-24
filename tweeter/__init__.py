06fa0317aff310093fbdb792ea437f056bde39f7
Ahmed Javed <ahmedjaved701@gmail.com>
2020-08-25 01:33:05 +0500

    Bug fixes

--- tweeter/__init__.py
+++ tweeter/__init__.py
@@ -7,8 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dcef3fd2f3a917dda52f56ed46ddea37'
-app.config['DATABASE_URL'] = 'postgresql:///tweeter'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tweeter'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
